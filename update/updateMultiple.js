19d2d48529feed5e6134eefb1f3a5d3f8b5c92e8
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-10-18 12:34:25 +0100

    update many objects

---
+++ update/updateMultiple.js
@@ -0,0 +1,56 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/drivers/node/ for more details
+     */
+     const uri = process.env.MONGODB_URL;
+     
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // UPDATE MANY
+        // Update all listings so they have a property type
+        await updateAllListingsToHavePropertyType(client);
+        // Print the details of the Testing rooms listing that should now have a property type
+        await findListingByName(client, "Testing rooms");
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function updateAllListingsToHavePropertyType(client) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateMany({ property_type: { $exists: false } }, { $set: { property_type: "house" } });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+async function findListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
+
+    if (result) {
+        console.log(`Found a listing in the db with the name '${nameOfListing}':`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+
